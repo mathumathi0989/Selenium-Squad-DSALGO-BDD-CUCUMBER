0362482e731d1142a50e944f1cd3adc72a0b0afd
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-07-24 23:19:48 -0400

    updated pom file

---
+++ src/test/java/stepdefinition/loginStepDefinition.java
@@ -0,0 +1,26 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginStepDefinition {
+
+	@Given("click on login")
+	public void click_on_login() {
+	   System.out.println("clicked login");
+	}
+	@When("user enters uname and pwd")
+	public void user_enters_uname_and_pwd() {
+	   System.out.println("entered username and password");
+	}
+	@When("click on login button")
+	public void click_on_login_button() {
+	   System.out.println("clciked login button");
+	}
+	@Then("dashboard page appears")
+	public void dashboard_page_appears() {
+	   System.out.println("dashboard page appeared");
+	}
+	
+}
