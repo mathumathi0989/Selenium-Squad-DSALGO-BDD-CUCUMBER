b0faf8203b9d70d03c037e658c94bec3b2b61ea8
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-07-26 12:01:17 -0400

    cucumber test run

--- src/test/java/stepdefinition/loginStepDefinition.java
+++ src/test/java/stepdefinition/loginStepDefinition.java
@@ -7,20 +7,23 @@ import io.cucumber.java.en.When;
 public class loginStepDefinition {
 
 	@Given("click on login")
-	public void click_on_login() {
-	   System.out.println("clicked login");
-	}
-	@When("user enters uname and pwd")
-	public void user_enters_uname_and_pwd() {
-	   System.out.println("entered username and password");
-	}
-	@When("click on login button")
-	public void click_on_login_button() {
-	   System.out.println("clciked login button");
-	}
-	@Then("dashboard page appears")
-	public void dashboard_page_appears() {
-	   System.out.println("dashboard page appeared");
-	}
+    public void click_on_login() {
+        System.out.println("clicked login");
+    }
+
+    @When("user enters uname and pwd")
+    public void user_enters_uname_and_pwd() {
+        System.out.println("entered username and password");
+    }
+
+    @When("click on login button")
+    public void click_on_login_button() {
+        System.out.println("clicked login button");
+    }
+
+    @Then("dashboard page appears")
+    public void dashboard_page_appears() {
+        System.out.println("dashboard page appeared");
+    }
 	
 }
