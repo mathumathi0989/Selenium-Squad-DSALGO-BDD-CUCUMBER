a099edc63c8e76657dc0e029dfb8497be0c9f90d
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-08-12 23:45:47 +0530

    array completed

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/Graph.feature"},
+        features = {"src/test/resources/features/Array.feature"},
         glue = {"stepdefinition","hooks"},
         plugin = {"pretty", "html:target/cucumber-reports.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestRunner extends AbstractTestNGCucumberTests {
