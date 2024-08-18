f2b100bfbb649b243a1eb0122b514ce1a59d2236
Anusuya Selvaraj <anusuyas.official@gmail.com>
2024-08-18 17:53:46 -0500

    commit

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = {"src/test/resources/features/06_Graph.feature"},
+        features = {"src/test/resources/features/05_Array.feature"},
         glue = {"stepdefinition","hooks"},
         plugin = {"pretty", "html:target/cucumber-reports.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestRunner extends AbstractTestNGCucumberTests {
