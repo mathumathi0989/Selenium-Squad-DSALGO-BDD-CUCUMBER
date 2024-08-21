d8e0afc571eeee941c08dd791230779b221fb907
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-08-21 20:45:12 +0530

    aug 21

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -6,28 +6,15 @@ import org.testng.annotations.Test;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-
-
-        features = {"src/test/resources/features"},
-
-        glue = {"stepdefinition","hooks"},
-        tags =  "@sanity" ,
-        plugin = {"pretty", "html:target/cucumber-reports.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+@CucumberOptions(features = { "src/test/resources/features" }, glue = { "stepdefinition",
+		"hooks" }, tags = "@sanity", plugin = { "pretty", "html:target/cucumber-reports.html",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 public class TestRunner extends AbstractTestNGCucumberTests {
-
 	@Override
 	@DataProvider(parallel = false)
 	public Object[][] scenarios() {
-	return super.scenarios();
+		return super.scenarios();
 	}
 
-
-	 
-	
-	
-	
-	
-	
-	
-	}
+}
