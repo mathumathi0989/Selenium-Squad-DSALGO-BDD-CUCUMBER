6c116c852ce63d5ab2b4a4f248f0840e9501ca14
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-08-24 08:01:13 +0530

    only rgression

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { "src/test/resources/features" }, glue = { "stepdefinition",
-		"hooks" }, tags = "@sanity", plugin = { "pretty", "html:target/cucumber-reports.html",
+		"hooks" }, tags = "@regression", plugin = { "pretty", "html:target/cucumber-reports.html",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" ,"rerun:target/failedrerun.txt"})
 
